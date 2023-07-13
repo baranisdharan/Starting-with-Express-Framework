3a000819cbed77401c39262449b75b98f12abc85
baranisdharan <110192626+baranisdharan@users.noreply.github.com>
2023-07-14 00:30:53 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -3,28 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res) => {
-    res.send(`
-        <form method="POST" action="/product">
-            <input type="text" name="title" placeholder="Product Title">
-            <input type="text" name="size" placeholder="Product Size">
-            <button type="submit">Add Product</button>
-        </form>
-    `);
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res) => {
-    const { title, size } = req.body;
-    console.log('Product Name:', title);
-    console.log('Product Size:', size);
-    res.redirect('/');
-});
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello</h1>');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
